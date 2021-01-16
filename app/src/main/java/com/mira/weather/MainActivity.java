50dee041e9db7c943f3ebb046244991523ae9d9a
Mira <mtorbay@tfs.ca>
2021-01-15 21:11:21 -0500

    Version 2

--- app/src/main/java/com/mira/weather/MainActivity.java
+++ app/src/main/java/com/mira/weather/MainActivity.java
@@ -2,6 +2,7 @@ package com.mira.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -13,14 +14,18 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtDate, txtCity, txtTemp, txtMinTemp, txtMaxTemp, txtTempFL, txtPressure, txtHumidity, txtSeaLevel, txtGroundLevel;
+    TextView txtDate, txtCity, txtTemp, txtMinTemp, txtMaxTemp, txtTempFL, txtPressure, txtHumidity, txtVisibility, txtDescription;
     ImageView imgIcon;
     String myCity="Toronto";
 
@@ -37,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
         txtTempFL=findViewById(R.id.txtTempFL);
         txtPressure=findViewById(R.id.txtPressure);
         txtHumidity=findViewById(R.id.txtHumidity);
-        txtSeaLevel=findViewById(R.id.txtSeaLevel);
-        txtGroundLevel=findViewById(R.id.txtGroundLevel);
+        txtDescription=findViewById(R.id.txtDescription);
+        txtVisibility=findViewById(R.id.txtVisibility);
+        imgIcon=findViewById(R.id.imgIcon);
 
         afficher();
     }
@@ -52,8 +58,52 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject main_object=response.getJSONObject("main");
                     JSONArray array=response.getJSONArray("weather");
-                    Log.d("weather", "resultat ="+array.toString());
-                    Log.d("main", "resultat ="+main_object.toString());
+                    // Log.d("weather", "resultat ="+array.toString());
+                    // Log.d("main", "resultat ="+main_object.toString());
+                    JSONObject object=array.getJSONObject(0);
+
+                    //temp
+                    int tempC=(int)Math.round(main_object.getDouble("temp"));
+                    String temp=String.valueOf(tempC);
+
+                    int temp_FL=(int)Math.round(main_object.getDouble("feels_like"));
+                    String tempFL=String.valueOf(temp_FL);
+
+                    int temp_min=(int)Math.round(main_object.getDouble("temp_min"));
+                    String tempMin=String.valueOf(temp_min);
+
+                    int temp_max=(int)Math.round(main_object.getDouble("temp_max"));
+                    String tempMax=String.valueOf(temp_max);
+
+                    // description, pressure, humidity, city, icon
+                    String description=object.getString("description");
+                  //  String pressure=object.getString("pressure");
+                    String humidity=object.getString("humidity");
+                    String visibility=object.getString("visibility");
+                    String city=response.getString("name");
+                    String icon=object.getString("icon");
+
+                    txtCity.setText(city);
+                    txtTemp.setText(temp+"°C");
+                    txtMinTemp.setText("Min: "+tempMin+"°C");
+                    txtMaxTemp.setText("Max: "+tempMax+"°C");
+                    txtTempFL.setText("Feels like: "+tempFL+"°C");
+                    txtDescription.setText(description);
+                   // txtPressure.setText("Pressure: "+pressure+" mbar");
+                    txtHumidity.setText("Humidity: "+humidity+"%");
+                    txtVisibility.setText("Visibility: "+visibility+" m");
+
+                    Calendar calendar=Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat=new SimpleDateFormat("EEEE, MMMM dd");
+                    String formatted_date=simpleDateFormat.format(calendar.getTime());
+
+                    txtDate.setText(formatted_date);
+
+                    //image
+                    String imageUri="http://openweathermap.org/img/w/"+ icon+"png";
+                    imgIcon=findViewById(R.id.imgIcon);
+                    Uri myUri=Uri.parse(imageUri);
+                    Picasso.with(MainActivity.this).load(myUri).resize(200, 200).into(imgIcon);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
